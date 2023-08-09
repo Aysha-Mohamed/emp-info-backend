4b78a46835083f417cae9c2356b4d26c649b319b
Aysha <42186490+Aysha-Mohamed@users.noreply.github.com>
2023-08-10 01:06:03 +0200

    Initial commit with backend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3010;
+
+app.use(cors());
+
+app.get('/api/employees', (req, res) => {
+  console.log('Received API request');
+  res.json({ message: 'API response from server' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend API listening on port ${PORT}`);
+});
