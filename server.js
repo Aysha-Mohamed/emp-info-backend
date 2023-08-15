d634f2f1978ac05cbbf075d0d1d25bc3a17759bc
Aysha <42186490+Aysha-Mohamed@users.noreply.github.com>
2023-08-15 03:38:10 +0200

    package.json updated due to heroku error

--- server.js
+++ server.js
@@ -6,10 +6,28 @@ const PORT = process.env.PORT || 3010;
 
 app.use(cors());
 
-app.get('/api/employees', (req, res) => {
-  console.log('Received API request');
-  res.json({ message: 'API response from server' });
-});
+app.get('/api/employees', async (req, res) => {
+    console.log('Received API request');
+  
+    try {
+      const url = 'https://api.1337co.de/v3/employees';
+      const headers = {
+        Authorization: 'api-key 14:2023-08-08:anna.vanduijvenbode@tretton37.com e45a016060a6a978619e585f261cc4f5692b2b8f96fad8ba45d2ae0435fc9cdd',
+      };
+  
+      const response = await fetch(url, { headers });
+      if (response.ok) {
+        const data = await response.json();
+        res.status(200).json(data);
+      } else {
+        console.log('Error fetching data from external API:', response.statusText);
+        res.status(500).json({ error: 'An error occurred while fetching data' });
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'An error occurred while fetching data' });
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Backend API listening on port ${PORT}`);
